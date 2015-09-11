0c2f8b794a140d310c91a15a78345cc79e5a0a63
Thiago <thgcode@users.noreply.github.com>
2015-09-11 20:42:46 -0300

    Added example.

---
+++ example.py
@@ -0,0 +1,19 @@
+from configurer import Configurer
+class Player(Configurer):
+    def __init__(self):
+        self.objects = []
+        unknown = self.load("example.ini", "player", P_INTS, P_STRINGS, P_BOOLEANS)
+        for k, v in unknown.iteritems():
+            if k.startswith("obj"):
+                self.objects.append(v)
+
+    def print_stats(self):
+        print "%s is level %d, his died flag is %d and his objects are:" % (self.name, self.level, self.died)
+        for o in self.objects:
+            print o
+
+P_INTS = ["level"]
+P_STRINGS = ["name"]
+P_BOOLEANS = ["died"]
+p = Player()
+p.print_stats()
