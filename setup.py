78bb27e361ead2d0926fbd880445e4a6b9754507
Thiago <thgcode@users.noreply.github.com>
2016-09-13 13:58:11 -0300

    Added a setup script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="configurer",
+    version="0.1",
+    url="https://github.com/thgcode/configurerclass",
+    description="Setts a class' attributes using an INI file",
+    author="Thiago",
+    author_email="thiago.seus@yahoo.com.br",
+    py_modules=["configurer"])
