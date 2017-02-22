04195441c9bfa9ae82a15ecd821398cd4cff73f5
Thiago <thgcode@users.noreply.github.com>
2017-02-22 11:12:11 -0300

    Fixed some stylistic errors and made the code work in both Python 2 and 3.

--- configurer.py
+++ configurer.py
@@ -1,21 +1,26 @@
-import ConfigParser
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+try:
+    from configparser import ConfigParser, NoOptionError
+except ImportError:
+    from ConfigParser import ConfigParser, NoOptionError
 class Configurer(object):
     def load(self, cfgfile, section, ints, strings, booleans=[]):
-        c=ConfigParser.ConfigParser()
+        c = ConfigParser()
         c.read(cfgfile)
         toparse = [] + ints + strings + booleans
         for o in toparse:
             try:
                 if o in ints:
-                    val=c.getint(section, o)
+                    val = c.getint(section, o)
                 elif o in strings:
-                    val=c.get(section, o)
+                    val = c.get(section, o)
                 elif o in booleans:
-                    val=c.getboolean(section, o)
+                    val = c.getboolean(section, o)
                 setattr(self, o, val)
-            except ConfigParser.NoOptionError:
+            except NoOptionError:
                 if not hasattr(self, o):
-                    raise ValueError
+                    raise ValueError("Missing option in the configuration file: %s" % o)
         unknown_items = {}
         for item, val in c.items(section):
             if item in toparse:
@@ -25,11 +30,11 @@ class Configurer(object):
         return unknown_items
 
     def save(self, cfgfile, section, ints, strings, booleans):
-        c=ConfigParser.ConfigParser()
+        c = ConfigParser()
         c.add_section(section)
         options = [] + ints + strings + booleans
         for o in options:
             val = getattr(self, o)
-            c.set(section, o, val)
-        f = open(cfgfile, "w")
+            c.set(section, o, str(val))
+        f = open(cfgfile, "wt")
         return c.write(f)
--- example.py
+++ example.py
@@ -1,16 +1,18 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 from configurer import Configurer
 class Player(Configurer):
     def __init__(self):
         self.objects = []
         unknown = self.load("example.ini", "player", P_INTS, P_STRINGS, P_BOOLEANS)
-        for k, v in unknown.iteritems():
+        for k, v in unknown.items():
             if k.startswith("obj"):
                 self.objects.append(v)
 
     def print_stats(self):
-        print "%s is level %d, his died flag is %d and his objects are:" % (self.name, self.level, self.died)
+        print("%s is level %d, his died flag is %d and his objects are:" % (self.name, self.level, self.died))
         for o in self.objects:
-            print o
+            print(o)
 
     def save_data(self):
         return self.save("another.ini", "player", P_INTS, P_STRINGS, P_BOOLEANS)
